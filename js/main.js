0e4564d261ce057fe9a93315a33ec4c31ae6bfae
David Vargas <dv.carrillo@yahoo.com>
2020-04-17 16:15:57 +0200

    Set random background img

--- js/main.js
+++ js/main.js
@@ -9,7 +9,7 @@ setRandomHeaderBackground = () => {
     let randNumber = Math.floor(Math.random() * 6);
     let headerElement;
     if(headerElement = document.getElementById("home-header")) {
-        headerElement.style.background = `url(../img/home-bg/${randNumber}.jpg) no-repeat center`;
+        headerElement.style.background = `url(https://raw.githubusercontent.com/dvcarrillo/portfolio-bootstrap/master/img/home-bg/${randNumber}.jpg) no-repeat center`;
     }
 
 }
