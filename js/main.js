29ca490e0282403aa45726a7b7aaa92ae17d129c
David Vargas <dv.carrillo@yahoo.com>
2020-04-17 17:33:57 +0200

    Small corrections

--- js/main.js
+++ js/main.js
@@ -7,12 +7,13 @@ $(document).ready(function () {
 
 setRandomHeaderBackground = () => {
     let randNumber = Math.floor(Math.random() * BG_IMAGES);
+    let imgUrl = `https://raw.githubusercontent.com/dvcarrillo/portfolio-bootstrap/master/img/home-bg/${randNumber}.jpg`;
+    
     let headerElement;
     if (headerElement = document.getElementById("home-header")) {
-        headerElement.style.background = `url(https://raw.githubusercontent.com/dvcarrillo/portfolio-bootstrap/master/img/home-bg/${randNumber}.jpg)`;
+        headerElement.style.background = `url(${imgUrl})`;
         headerElement.style.backgroundPosition = 'center top';
         headerElement.style.backgroundRepeat = 'no-repeat';
         headerElement.style.backgroundSize = 'cover';
     }
-
 }
