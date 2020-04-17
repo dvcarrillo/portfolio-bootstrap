4ec8aaf38f758add4d09bb9f706ce8415d0a724e
David Vargas <dv.carrillo@yahoo.com>
2020-04-17 16:28:23 +0200

    Correct background issues

--- js/main.js
+++ js/main.js
@@ -1,15 +1,16 @@
 let BG_IMAGES = 5;
 
-window.onload = () => {
+$(document).ready(function () {
     console.log("window loaded");
     setRandomHeaderBackground();
-}
+});
 
 setRandomHeaderBackground = () => {
     let randNumber = Math.floor(Math.random() * 6);
     let headerElement;
-    if(headerElement = document.getElementById("home-header")) {
-        headerElement.style.background = `url(https://raw.githubusercontent.com/dvcarrillo/portfolio-bootstrap/master/img/home-bg/${randNumber}.jpg) no-repeat center`;
+    if (headerElement = document.getElementById("home-header")) {
+        headerElement.style.background = `url(https://raw.githubusercontent.com/dvcarrillo/portfolio-bootstrap/master/img/home-bg/${randNumber}.jpg) no-repeat center top`;
+        headerElement.style.backgroundSize = '100%';
     }
 
 }
