7ee165a9c26a5eb85ec5384e369903576bd0bc67
David Vargas <dv.carrillo@yahoo.com>
2020-04-19 02:02:43 +0200

    Variable footer message

--- js/page-default.js
+++ js/page-default.js
@@ -1,2 +1,14 @@
+const footerSentences = [
+    'like what you see?',
+    'does it look appealing?',
+    'why don\'t we talk?',
+    'want to know more?'
+]
+
 $(document).ready(function () {
+    setRandomFooterSentence();
 });
+
+setRandomFooterSentence = () => {
+    document.querySelector('#footer-title-sentence').innerHTML = footerSentences[Math.floor(Math.random() * footerSentences.length)];
+}
